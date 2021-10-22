01c0d8dfee9bde490959da0387863f339a3d6520
Chris McGee <cmac0351@gmail.com>
2021-10-22 13:55:55 -0500

    refactored api export method

--- client/src/APIutils.js
+++ client/src/APIutils.js
@@ -1,10 +1,13 @@
-module.exports = {
-  getTestData: async () => {
-    const response = await fetch('/api');
-    const body = await response.json();
-    if (response.status !== 200) {
-      throw Error(body.message)
-    }
-    return body;
+
+const getTestData = async () => {
+  const response = await fetch('/api');
+  const body = await response.json();
+  if (response.status !== 200) {
+    throw Error(body.message)
   }
+    return body;
+}
+
+export {
+  getTestData,
 }
