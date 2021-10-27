ed47ab7aa5bcbe42c49b819fba04924485b1bf9e
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-10-27 09:08:10 -0700

    commit

--- client/src/components/Rider/MapContainer.jsx
+++ client/src/components/Rider/MapContainer.jsx
@@ -1,15 +1,37 @@
-import React from 'react';
-import GoogleMap from 'google-map-react';
+import React, { useState, useEffect } from 'react';
+import GoogleMapReact from 'google-map-react';
+import Geocode from "react-geocode";
+
+Geocode.setApiKey('AIzaSyAxt1bss46Tsxo2ko683AGyx4U2sauBX4o');
+Geocode.setRegion('us');
 
 const MapContainer = (props) => {
+    const [riderLatLng, setRiderLatLng] = useState({lat: 37.658428, lng: -121.876999});
+
+    useEffect(() => {
+        Geocode.fromAddress(props.riderLocation).then(
+            (response) => {
+                const { lat, lng } = response.results[0].geometry.location;
+                setRiderLatLng({lat: lat, lng: lng});
+                console.log('🌻', riderLatLng);
+            },
+            (error) => {
+                console.error(error);
+            }
+        );
+    });
+    
+    const Marker = ({text}) => <div>{text}</div>
+    
     return (
         <div style={{ height: '65vh', width: '100%' }}>
-        <GoogleMap
+            <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyAxt1bss46Tsxo2ko683AGyx4U2sauBX4o' }}
             defaultCenter={props.center}
             defaultZoom={props.zoom}
-        >
-        </GoogleMap>
+            >
+                <Marker lat={riderLatLng.lat} lng={riderLatLng.lng} text="G" />
+            </GoogleMapReact>
         </div>
     );
 }
--- client/src/components/Rider/Rider.jsx
+++ client/src/components/Rider/Rider.jsx
@@ -4,12 +4,13 @@ import MapContainer from './MapContainer';
 const Rider = (props) => {
   const [center, setCenter] = useState({lat: 37.658428, lng: -121.876999});
   const [zoom, setZoom] = useState(11);
-  // more state examples: pickup locations, destinations, user's location (or maybe not) 
+  const [riderLocation, setRiderLocation] = useState(94588);
+  // more state examples: pickup locations, destinations, user's location
 
   return (
     <div>
       <button onClick={props.riderHandle}>BACK</button>
-      <MapContainer center={center} zoom={zoom} />
+      <MapContainer riderLocation={riderLocation} center={center} zoom={zoom} />
     </div>
   );
 };
