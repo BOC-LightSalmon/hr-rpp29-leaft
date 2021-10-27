31fe46a15501cc49f3fc0516a1d9f6165a803004
Chris McGee <cmac0351@gmail.com>
2021-10-26 23:28:58 -0500

    using env variable for db password

--- db/config.js
+++ db/config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('leaft', 'root', '', {
+const db = new Sequelize('leaft', 'root', process.env.DB_PASSWORD, {
   dialect: 'mysql',
   host: process.env.DB_HOSTNAME,
   port: 3306,
