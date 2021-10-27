bf7a3c82c18bdabf4127d7729d2c0d3630c91bae
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-10-27 14:50:00 -0700

    comething

--- client/src/components/Rider/MapContainer.jsx
+++ client/src/components/Rider/MapContainer.jsx
@@ -6,34 +6,52 @@ Geocode.setApiKey('AIzaSyAxt1bss46Tsxo2ko683AGyx4U2sauBX4o');
 Geocode.setRegion('us');
 
 const MapContainer = (props) => {
-    const [riderLatLng, setRiderLatLng] = useState({lat: 37.658428, lng: -121.876999});
+    const [riderLatLng, setRiderLatLng] = useState({});
+    const [pickupsLatLng, setPickupsLatLng] = useState([]); // array of lats and lngs of all the nearby pickups like [{lat: , lng: }, {lat: , lng: }]
 
+    console.log('🦔', props.nearbyRides);
     useEffect(() => {
         Geocode.fromAddress(props.riderLocation).then(
             (response) => {
                 const { lat, lng } = response.results[0].geometry.location;
                 setRiderLatLng({lat: lat, lng: lng});
-                console.log('🌻', riderLatLng);
             },
             (error) => {
                 console.error(error);
             }
         );
-    });
+        console.log('🦥', riderLatLng)
+        let array = [];
+        props.nearbyRides.forEach((nearbyRide) => {
+                Geocode.fromAddress(nearbyRide.start).then((response) => {
+                    const { lat, lng } = response.results[0].geometry.location;
+                    array.push({lat: lat, lng: lng});
+                    setPickupsLatLng(array);
+                },
+                (error) => {
+                    console.error(error);
+                }
+            );
+        });
+        console.log('🌺', pickupsLatLng);
+    }, []);
     
     const Marker = ({text}) => <div>{text}</div>
     
     return (
-        <div style={{ height: '65vh', width: '100%' }}>
+        <div style={{ height: '50vh', width: '100%' }}>
             <GoogleMapReact
             bootstrapURLKeys={{ key: 'AIzaSyAxt1bss46Tsxo2ko683AGyx4U2sauBX4o' }}
             defaultCenter={props.center}
             defaultZoom={props.zoom}
             >
-                <Marker lat={riderLatLng.lat} lng={riderLatLng.lng} text="G" />
+                <Marker lat={riderLatLng.lat} lng={riderLatLng.lng} text='🍁' />
+                {pickupsLatLng.map((pickupLatLng) => <Marker lat={pickupLatLng.lat} lng={pickupLatLng.lng} text='🌳' />)}
             </GoogleMapReact>
         </div>
     );
 }
 
 export default MapContainer;
+
+// on the first page we will need all the pickup locations in a given zipcode
--- client/src/components/Rider/Rider.jsx
+++ client/src/components/Rider/Rider.jsx
@@ -1,16 +1,21 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import MapContainer from './MapContainer';
 
 const Rider = (props) => {
   const [center, setCenter] = useState({lat: 37.658428, lng: -121.876999});
   const [zoom, setZoom] = useState(11);
   const [riderLocation, setRiderLocation] = useState(94588);
-  // more state examples: pickup locations, destinations, user's location
+  const [nearbyRides, setNearbyRides] = useState([]); // this will be an array with all the columns (or not) of the Routes table, it will have all the info including pick up and dropoff locations, times where the riderLocation is equal to Ride.zip
+
+  // will need useEffect to fetch data from database and change state accordingly
+  useEffect(() => {
+    setNearbyRides([{start: 'muirwood park', zip: '94588'}, {start: 'moller park', zip: '94588'}]);
+  }, []);
 
   return (
     <div>
       <button onClick={props.riderHandle}>BACK</button>
-      <MapContainer riderLocation={riderLocation} center={center} zoom={zoom} />
+      <MapContainer nearbyRides={nearbyRides} riderLocation={riderLocation} center={center} zoom={zoom} />
     </div>
   );
 };
