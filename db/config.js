96ab8efddcfd9aaeaed6a8764666e709b63d0632
Ryan Lott <ryanclott@outlook.com>
2021-10-28 16:35:48 -0500

    Included db password in connection

--- db/config.js
+++ db/config.js
@@ -1,10 +1,10 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const db = new Sequelize('leaft', 'root', '', {
+const db = new Sequelize('leaft', 'root', process.env.DB_PASSWORD, {
   dialect: 'mysql',
   host: process.env.DB_HOSTNAME,
-  port: 3306,
+  port: 3306
 });
 
 module.exports = db;
