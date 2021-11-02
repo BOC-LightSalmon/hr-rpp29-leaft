805910337233b15f88c05bd5c65524b363e138e3
Salma Noe <familypc@Salmas-Macbook-Air.local>
2021-11-01 20:41:37 -0700

    rides list is now connected to database

--- client/src/components/Rider/SelectedRide.jsx
+++ client/src/components/Rider/SelectedRide.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 
 const SelectedRide = props => {
-    
+    console.log('🌺', props);
     return (
         <div className={"RiderBtmPanel"} id="SelectedRide">
             <div id="confirmationPage">
                 <span id="confirmationPageHeader">{!props.rideConfirmed ? 'Here are the ride\'s details:' : 'Ride details:'}</span>
                 <ul id="fullRideDetails">
-                    <li><span className="rideDetail">Pick up time: </span>{props.ride.time ? props.ride.time: 'Tuesday, Nov 18th at 9am'}</li>
-                    <li><span className="rideDetail">Pick up location: </span>{props.ride.start}</li>
-                    <li><span className="rideDetail">Drop off location: </span>{props.ride.end}</li>
+                    <li><span className="rideDetail">Pick up time: </span>{props.ride.date ? props.ride.date: 'Tuesday, Nov 18th at 9am'}</li>
+                    <li><span className="rideDetail">Pick up location: </span>{props.ride.pickUp}</li>
+                    <li><span className="rideDetail">Drop off location: </span>{props.ride.dropOff}</li>
                     <li><span className="rideDetail">Driver's Name: </span>{props.ride.driver ? props.ride.driver: 'Nate'}</li>
                 </ul>
             </div>
