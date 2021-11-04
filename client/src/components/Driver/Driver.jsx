b494ddab12586d7b89d697813a2544f2bf0e5fd2
Chris McGee <cmac0351@gmail.com>
2021-11-04 15:08:05 -0500

    added user as context to components

--- client/src/components/Driver/Driver.jsx
+++ client/src/components/Driver/Driver.jsx
@@ -5,6 +5,7 @@ import Map from './Map';
 import RoutesList from './RoutesList';
 import './driver.scss';
 import Navbar from '../Navbar/Navbar';
+import { AuthContext } from '../../App';
 
 class Driver extends React.Component {
   constructor(props) {
@@ -75,19 +76,33 @@ class Driver extends React.Component {
   }
 
   render() {
-    console.log(this.props)
     if (this.state.loaded) {
-      return(
-        <div id="driver-container">
-          <Navbar userId={this.props.userId} />
-          {/* <p><i onClick={this.props.driverHandle} className="arrow left"></i></p> */}
-          <div id="driver-wrapper">
-            <Map routes={this.state.routes} selectedRoute={this.state.selectedRoute} />
-            <RoutesList routes={this.state.routes} getRoutes={this.getRoutes} driverName={this.state.driverName} selectRoute={this.selectRoute} />
-            <button onClick={this.showForm} id="make-new-route">Make New Route</button>
-          </div>
-          {this.state.modal && <RouteForm getRoutes={this.getRoutes} closeForm={this.closeForm}/>}
-        </div>
+      return (
+        <AuthContext.Consumer >
+          {userData => { // userData is App state
+           return (
+             <div id="driver-container">
+              {/* 
+              *****************************************************
+
+              You can access anything from App state like this:
+               
+              <div>{userData.id}</div> 
+               
+              *****************************************************
+              */}
+
+              <Navbar />
+              <div id="driver-wrapper">
+                <Map routes={this.state.routes} selectedRoute={this.state.selectedRoute} />
+                <RoutesList routes={this.state.routes} getRoutes={this.getRoutes} driverName={this.state.driverName} selectRoute={this.selectRoute} />
+                <button onClick={this.showForm} id="make-new-route">Make New Route</button>
+              </div>
+              {this.state.modal && <RouteForm getRoutes={this.getRoutes} closeForm={this.closeForm}/>}
+            </div>
+           )
+          }}
+        </AuthContext.Consumer>
       );
     } else {
       return(
