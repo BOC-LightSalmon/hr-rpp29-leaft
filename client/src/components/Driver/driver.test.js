faa7915cb99ffab9100eac2ecb1539a7c3e4bf36
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-06 12:33:32 -0700

    fleshed out driver tests

--- client/src/components/Driver/driver.test.js
+++ client/src/components/Driver/driver.test.js
@@ -1,4 +1,28 @@
-describe('dummy test', () => {
+describe('<Driver />', () => {
+  it ('should pass', () => {
+    expect(1).toEqual(1);
+  });
+});
+
+describe('Map />', () => {
+  it ('should pass', () => {
+    expect(1).toEqual(1);
+  });
+});
+
+describe('<RoutesList />', () => {
+  it ('should pass', () => {
+    expect(1).toEqual(1);
+  });
+});
+
+describe('<RouteForm />', () => {
+  it ('should pass', () => {
+    expect(1).toEqual(1);
+  });
+});
+
+describe('<Table />', () => {
   it ('should pass', () => {
     expect(1).toEqual(1);
   });
