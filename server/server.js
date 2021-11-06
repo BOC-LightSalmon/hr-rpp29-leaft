b9807e842de8f2424731526c46ae861f2bc66ac2
Chris McGee <57733662+cmac0351@users.noreply.github.com>
2021-11-06 13:19:07 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.REACT_APP_SERVER_PORT;
+const port = process.env.PORT;
 const http = require('http');
 require('../db/index');
 
