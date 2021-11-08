5ffb8d43aa6e89693b1bddf0a8b3d6fea37d80fe
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-08 15:11:08 -0800

    fleshing out driver component tests

--- client/src/components/Driver/driver.test.js
+++ client/src/components/Driver/driver.test.js
@@ -4,9 +4,27 @@ import { AuthContext } from '../../App';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
 
+import Driver from './Driver';
+
 describe('<Driver />', () => {
-  it ('should pass', () => {
-    expect(1).toEqual(1);
+  let Component;
+
+  beforeEach(() => {
+    Component = render(
+      <AuthContext.Provider value={{ test: 'test' }}>
+        <Router>
+          <Driver userId={1}/>
+        </Router>
+      </AuthContext.Provider>
+    );
+  });
+
+  it('should render', () => {
+    expect(Component).toBeTruthy();
+  });
+
+  it('should display "Loading..." if routes not fetched yet', () => {
+    expect(screen.getByText('Loading...')).toBeInTheDocument();
   });
 });
 
