fe01f30330cce216712f5521610ebbb8521512cf
Chris McGee <cmac0351@gmail.com>
2021-11-08 11:26:48 -0600

    removed commmented and old code

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-// import SignUp from './components/SignUp.jsx';
 import Main from './components/Main.jsx';
 import Login from './components/SignUp/login.jsx';
 import Register from './components/SignUp/register.jsx';
@@ -10,22 +9,15 @@ import PrivateRoute from './components/PrivateRoute.jsx';
 import BalanceTransfer from './components/Balance/BalanceTransfer.jsx';
 import BalanceUpdate from './components/Balance/BalanceUpdate.jsx';
 import './App.scss';
-import axios from 'axios'
 
 export const AuthContext = React.createContext();
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
-    this.signUpHandle = this.signUpHandle.bind(this);
-    this.loginHandle = this.loginHandle.bind(this);
-    this.handleRedirect = this.handleRedirect.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleBalanceUpdate = this.handleBalanceUpdate.bind(this);
     this.state = {
-      signUp: false,
-      login: false,
-      redirect: '/register',
       email: this.props.data?.email || '',
       first_name: this.props.data?.first_name || '',
       last_name: this.props.data?.last_name ||'',
@@ -35,7 +27,6 @@ class App extends React.Component {
 
   }
   handleLogin(data) {
-    console.log(data)
     for(var keys in data) {
       this.setState({
         [keys]: data[keys] === null ? 0 : data[keys]
@@ -43,57 +34,22 @@ class App extends React.Component {
     }
   }
 
-  handleRedirect(redirect) {
+  handleBalanceUpdate(newBalance) {
     this.setState({
-      redirect: redirect
+      balance: newBalance
     })
   }
 
-  signUpHandle() {
-    this.setState({
-      signUp: !this.state.signUp
-    })
-  }
-
-  loginHandle() {
-    this.setState({
-      login: !this.state.login
-    })
-  }
-  // UNSAFE_componentWillMount() {
-  //   console.log('hi');
-  //   this.setState({
-  //     id: this.props.id
-  //   })
-  // }
 
   render() {
     const { id } = this.state;
-    console.log("iddddddddd",id);
-    console.log(this.state)
-    // const { signUp, login } = this.state;
 
-    // if(this.state.redirect === '/register') {
-    //   return(<Register redirect={this.handleRedirect} login={this.handleLogin}/>);
-    // }
-    // if(this.state.redirect === '/login') {
-    //   if(this.state.email !== '') {
-    //     return(<Login login={this.handleLogin} redirect={this.handleRedirect} isLoggedIn={true}/>);
-    //   }
-    //   return(<Login login={this.handleLogin} redirect={this.handleRedirect}/>);
-    // }
-    // if (signUp === true) {
-    //   return (<SignUp signUpHandle={this.signUpHandle}/>)
-    // }
-    // if (login === true) {
-    //   return (<Main loginHandle={this.loginHandle}/>)
-    // }
     return (
       <Router>
           <Switch>
 
             <Route exact path="/login">
-              <Login login={this.handleLogin} redirect={this.handleRedirect}/>
+              <Login login={this.handleLogin}/>
             </Route>
 
             <Route exact path="/register">
@@ -104,8 +60,8 @@ class App extends React.Component {
             <PrivateRoute exact path="/" component={Main} userId={id} />
             <PrivateRoute exact path="/driver" component={Driver} userId={id} />
             <PrivateRoute exact path="/rider" component={Rider} userId={id} />
-            <PrivateRoute exact path="/balance-update" component={BalanceUpdate} userId={id} />
-            <PrivateRoute exact path="/balance-transfer" component={BalanceTransfer} userId={id} />
+            <PrivateRoute exact path="/balance-update" component={BalanceUpdate} handleBalanceUpdate={this.handleBalanceUpdate} />
+            <PrivateRoute exact path="/balance-transfer" component={BalanceTransfer} />
           </AuthContext.Provider>
 
             <Route path="/">
