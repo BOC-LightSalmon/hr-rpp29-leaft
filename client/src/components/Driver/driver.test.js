c3e3127209b609c9c5766b66055687a8b30cc7aa
Sunik Kim <sunik.h.kim@gmail.com>
2021-11-08 13:55:31 -0800

    fleshing out driver tests

--- client/src/components/Driver/driver.test.js
+++ client/src/components/Driver/driver.test.js
@@ -1,10 +1,16 @@
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AuthContext } from '../../App';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+
 describe('<Driver />', () => {
   it ('should pass', () => {
     expect(1).toEqual(1);
   });
 });
 
-describe('Map />', () => {
+describe('<Map />', () => {
   it ('should pass', () => {
     expect(1).toEqual(1);
   });
