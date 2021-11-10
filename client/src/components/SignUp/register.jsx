21fa3176a64147c77aab2e147aa878d9333b0ed9
Richard Medina <medinarichard996@gmail.com>
2021-11-09 16:26:15 -0800

    Working on css

--- client/src/components/SignUp/register.jsx
+++ client/src/components/SignUp/register.jsx
@@ -64,6 +64,11 @@ class Register extends React.Component {
     }
     return(
       <div id="register">
+        <div className="wave">
+    <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+        <path d="M985.66,92.83C906.67,72,823.78,31,743.84,14.19c-82.26-17.34-168.06-16.33-250.45.39-57.84,11.73-114,31.07-172,41.86A600.21,600.21,0,0,1,0,27.35V120H1200V95.8C1132.19,118.92,1055.71,111.31,985.66,92.83Z" className="shape-fill"></path>
+    </svg>
+</div>
         <form onChange={() => {
           this.setState({
             errorMessage: ''
@@ -85,9 +90,10 @@ class Register extends React.Component {
           <input type='password' id='password' minLength='6' onChange={e => this.handleChange(e)} required></input>
           <br></br>
           <input className="register-buttons" type='submit' value='Submit'></input>
+          <h5 id="already-have-account">Already Have An Account?</h5>
+        <NavLink className="register-buttons" id='nav' to="/login">Sign In</NavLink>
         </form>
-        <h5 id="already-have-account">Already Have An Account?</h5>
-        <NavLink className="register-buttons" to="/login">Sign In</NavLink>
+
         {/* <button className="register-buttons" onClick={() => {
           this.props.redirect('/login')
         }}>Sign In</button> */}
