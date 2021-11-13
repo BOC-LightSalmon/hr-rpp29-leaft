b43080cb5c7aeab150610435e50cf9cdd6c514ff
Chris McGee <cmac0351@gmail.com>
2021-11-12 21:48:08 -0600

    set max width on inputs and used placeholders instead of labels

--- client/src/components/SignUp/login.jsx
+++ client/src/components/SignUp/login.jsx
@@ -1,9 +1,6 @@
 import React from 'react';
 import { NavLink, Redirect } from 'react-router-dom';
 import axios from 'axios';
-
-import Main from '../Main.jsx';
-
 import './login.scss';
 
 class Login extends React.Component {
@@ -31,15 +28,7 @@ class Login extends React.Component {
       email: e.target.value
     })
   }
-  // componentDidMount() {
-  //   axios.get('/api/logins/checkAuth').then(() => {
-  //     this.setState({
-  //       isLoggedIn: true
-  //     })
-  //   }).catch((err) => {
-  //     return
-  //   })
-  // }
+
   submitForm(e) {
     e.preventDefault();
     if(this.state.email === '' || this.state.password === '') {
@@ -50,8 +39,6 @@ class Login extends React.Component {
       this.setState({
         isLoggedIn: true
       })
-      // redirect to main
-
     })
     .catch((err) =>  {
       console.log(err.response);
@@ -71,19 +58,22 @@ class Login extends React.Component {
     </svg>
 </div>
         <div id="login-register">
-        <form onChange={() => {
-          this.setState({
-            errorMessage: ''
-          })
-        }} onSubmit={e => this.submitForm(e)}>
-          <label htmlFor='login_email'>Email:</label>
-          <input type='email' id='login_email' name='email' onChange={e => this.handleEmail(e)} required></input>
-          <br></br>
-          <label htmlFor='login_password'>Password:</label>
-          <input type='password' id='login_password' name='password' minLength='6' onChange={e => this.handlePassword(e)} required></input>
-          <br></br>
-          <input type='submit' value='Submit' id="login-submit-button"></input>
-        </form>
+          <h4>Login</h4>
+          <div id="login-form">
+            <form onChange={() => {
+              this.setState({
+                errorMessage: ''
+              })
+            }} onSubmit={e => this.submitForm(e)}>
+              {/* <label htmlFor='login_email'>Email:</label> */}
+              <input type='email' id='login_email' placeholder="Email:" name='email' onChange={e => this.handleEmail(e)} required></input>
+              <br></br>
+              {/* <label htmlFor='login_password'>Password:</label> */}
+              <input type='password' id='login_password' placeholder="Password:" name='password' minLength='6' onChange={e => this.handlePassword(e)} required></input>
+              <br></br>
+              <input type='submit' value='Submit' id="login-submit-button"></input>
+            </form>
+          </div>
         <div className='footer1'>
           <h5 id="new-here">New Here? <NavLink id="nav1" to="/register">Register</NavLink>
           </h5>
